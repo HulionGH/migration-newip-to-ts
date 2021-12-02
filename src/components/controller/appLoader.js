2361c64d735eb4cc3c8972af73cdcafdd91ccec7
HulionGH <julia.gramovich@mail.ru>
2021-12-02 13:52:53 +0300

    feat: add API-key to loader

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: 'e35bbece32c24b328a77955e2f7ec8db', // получите свой ключ https://newsapi.org/
         });
     }
 }
